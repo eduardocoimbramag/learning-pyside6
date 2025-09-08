03045d41fb2bc6135a447c3dceb2862cebc5c2bb
Eduardo Coimbra <eduardocoimbramag@gmail.com>
2025-09-08 20:19:14 -0300

    create-application

--- first-windows.py
+++ first-windows.py
@@ -1,12 +1,4 @@
 from PySide6.QtWidgets import QApplication, QWidget
 import sys
 
-# Cria a aplicação
 app = QApplication(sys.argv)
-
-# Cria a janela
-janela = QWidget()
-janela.show()
-
-# Executa o loop da aplicação
-app.exec()
